06d11f18b9b4fa78d85fe46323e42176bc9f0c2d
tazimmer <markus-zimmermann@stud.hslu.ch>
2011-03-16 17:28:58 +0100

    initial commit

---
+++ src/Client.java
@@ -0,0 +1,53 @@
+
+import java.net.*;
+import java.io.*;
+
+public class Client
+{
+	Client() throws IOException 
+	{
+		
+		InetAddress adress = Inet4Address.getByName("127.0.0.1");
+		System.out.println("client startet..");
+		
+		DatagramSocket socket = new DatagramSocket();
+		DatagramPacket packet = new DatagramPacket(new byte[]{65,66,67,68}, 4, adress, 1234);
+		socket.send(packet);
+		
+		System.out.println("gesendet, warte auf antwort...");
+		
+		byte[] buf = new byte[256];
+		DatagramPacket responsePacket = new DatagramPacket(buf, buf.length);
+		socket.receive(responsePacket);
+		
+		socket.close();
+		
+		String s = new String(buf);
+		System.out.println("empfangen: ");
+		System.out.println(s.trim());
+		
+		
+		
+		/**
+		InputStream input = server.getInputStream(); 
+		OutputStream output = server.getOutputStream();
+		output.write(5);
+		output.write(10);
+		output.flush();
+		System.out.println(input.read());
+		server.close();
+		input.close();
+		output.close();
+		*/
+
+	}
+
+	public static void main (String[] args) 
+	{
+		try {
+			Client client = new Client();
+		} catch (IOException e) {
+			System.out.print(e);
+		}
+	}
+}
---
+++ src/Server.java
@@ -0,0 +1,44 @@
+
+import java.net.*;
+import java.io.*;
+
+public class Server
+{
+	DatagramSocket socket = new DatagramSocket(1234);
+
+	Server() throws IOException 
+	{ 
+		System.out.println("server started..");
+		while (true)
+		{
+			byte[] buf = new byte[256];
+			DatagramPacket packet = new DatagramPacket(buf, buf.length);
+			socket.receive(packet);
+			System.out.println("empfangen: ");
+			String s = new String(buf);
+			System.out.println(s.trim());
+			
+			//antwort
+			InetSocketAddress address = (InetSocketAddress)packet.getSocketAddress();
+			System.out.println("von: "+address.getAddress());
+			int port = packet.getPort();
+			byte[] answBuf = "ACK".getBytes();
+			DatagramPacket answPacket = new DatagramPacket(answBuf, answBuf.length, address.getAddress(), port);
+			socket.send(answPacket);
+			System.out.println("Antwort gesendet");
+
+
+
+		}
+	}
+
+	public static void main (String[] args) 
+	{
+		try
+		{
+			Server server = new Server();
+		} catch (IOException e) { 
+			System.out.print(e);
+		}
+	}
+}
